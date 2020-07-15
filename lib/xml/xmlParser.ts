34350790cc99aa885541a4036ece81d0163d478e
Igor Savin <iselwin@gmail.com>
2020-07-15 14:04:28 +0200

    Cleanup

--- lib/xml/xmlParser.ts
+++ lib/xml/xmlParser.ts
@@ -1,6 +1,7 @@
+const { parseString } = require('xml2js')
+
 export function parseXml(xml: string): Promise<any> {
   return new Promise((resolve, reject) => {
-    const parseString = require('xml2js').parseString
     parseString(xml, function (err: Error, result: any) {
       if (err) {
         return reject(err)
